8fc975e2a820d69cf6fc0cd2962e5c757c62306c
Ali Polatel <hawking@gentoo.org>
2008-10-24 18:12:27 +0300

    Add initial manpage.

---
+++ doc/makeman.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+# vim: set sw=4 et sts=4 tw=80 :
+# Copyright 2008 Ali Polatel <polatel@itu.edu.tr>
+# Distributed under the terms of the GNU General Public License v2
+
+MANPAGE_XSL="/etc/asciidoc/docbook-xsl/manpage.xsl"
+
+asciidoc=$(type -P asciidoc 2>/dev/null)
+if [[ -z "$asciidoc" ]]; then
+    echo "asciidoc not in PATH" >&2
+    exit 1
+fi
+
+xsltproc=$(type -P xsltproc 2>/dev/null)
+if [[ -z "$xsltproc" ]]; then
+    echo "xsltproc not in PATH" >&2
+    exit 1
+fi
+
+if [[ ! -f "$MANPAGE_XSL" ]]; then
+    echo "$MANPAGE_XSL doesn't exist" >&2
+    exit 1
+fi
+
+$asciidoc -d manpage -b docbook boogie.1.txt
+$xsltproc --nonet "$MANPAGE_XSL" boogie.1.xml
+rm -f boogie.1.xml
+
